b44de3b0b24275e7e4bf32015cb093af3f37b609
Tobias Jacobsen <tobias.cbs@gmail.com>
2016-04-20 11:38:38 +0200

    added usercontroller

--- ShoppingWebservice/Models/Category.cs
+++ ShoppingWebservice/Models/Category.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace ShoppingWebservice.Models {
     public class Category {
+
+        [Key]
+        public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
-        public List<Item> Items { get; set; } 
+        public List<Item> Items { get; set; }
 
         public Category() { }
 
